fee2b2a42d1a8b64e801413c74b9a117a85a159e
deeshavon <deja.greene@tlgcohort.com>
2022-09-20 10:06:36 -0700

    INITIALIZATION DEMONSTRATION

--- src/main/java/com/tlglearning/animals/Wolf.java
+++ src/main/java/com/tlglearning/animals/Wolf.java
@@ -2,6 +2,30 @@ package com.tlglearning.animals;
 
 public class Wolf {
 
+  private static final int MY_CONSTANT = initializeValue();
+
+  private int data = initializeData();  // <--*A FIELD*
+
+  static {
+    System.out.println("Wolf static initialization block"); //<--INSTANCE INITIALIZATION STATEMENT
+  }
+
+  {
+    System.out.println("Wolf instance initialization block"); //<--INSTANCE INITIALIZATION STATEMENT
+  }
+
+  public Wolf(){
+    System.out.println("Wolf constructor");
+  }
+
+  private static int initializeValue() {
+    System.out.println("Wolf.initializeValue");
+    return -1;
+  }
+  private int initializeData() {
+    System.out.println("Wolf.initializeData");
+    return -1;
+  }
  public void vocalize() {
    System.out.println("Howl like a wolf");
  }
